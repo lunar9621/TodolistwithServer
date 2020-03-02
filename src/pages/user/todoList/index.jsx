924420f8eb513a14215ce17cc60d2deb871ca7ca
lunar9621 <546604583@qq.com>
2020-03-02 16:41:02 +0800

    final

--- src/pages/user/todoList/index.jsx
+++ src/pages/user/todoList/index.jsx
@@ -1,11 +1,11 @@
-import {Input,Form,Table,Radio,Popconfirm,Button,message} from 'antd';
+import { Input, Form, Table, Radio, Popconfirm, Button, message } from 'antd';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import React, { Component } from 'react';
 import { Link } from 'umi';
 import { connect } from 'dva';
 import EditableCell from './components/EditableCell';
 import styles from './style.less';
-const {Search}=Input;
+const { Search } = Input;
 const EditableContext = React.createContext();
 
 const EditableRow = ({ form, index, ...props }) => (
@@ -16,22 +16,22 @@ const EditableRow = ({ form, index, ...props }) => (
 
 const EditableFormRow = Form.create()(EditableRow);
 @Form.create()
- class TodoList extends Component {
-  
+class TodoList extends Component {
+
   state = {
-    selectedRowKeys:[],
-    status:0,
+    selectedRowKeys: [],
+    status: 0,
   };
-  columns=[ {
+  columns = [{
     dataIndex: 'name',
     width: '60%',
     editable: true,
   },
   {
     dataIndex: 'operation',
-    width:'20%',
+    width: '20%',
     render: (text, record) =>
-      this.props.eventList.length >= 1 ? (
+      this.props.eventList && this.props.eventList.length > 0 ? (
         <Popconfirm title={formatMessage({
           id: 'Sure to delete?',
         })} onConfirm={() => this.handleDelete(record.index)}>
@@ -40,94 +40,95 @@ const EditableFormRow = Form.create()(EditableRow);
       ) : null,
   }]
 
-  componentDidMount(){
-   const {dispatch}=this.props;  
-   let tmpselectedRowKeys=[],flag=0;
-   dispatch({
-     type:'todos/fetchEventList',
-     payload:0,
-     callback:()=>{
-      for(let i=0;i<this.props.eventList.length;i++){
-        if(this.props.eventList[i].done===true){
-        tmpselectedRowKeys.push(flag);
+  componentDidMount() {
+    const { dispatch, eventList } = this.props;
+    let tmpselectedRowKeys = [], flag = 0;
+    dispatch({
+      type: 'todos/fetchEventList',
+      payload: 0,
+      callback: () => {
+        for (let i = 0; i < eventList.length; i++) {
+          if (eventList[i].done === true) {
+            tmpselectedRowKeys.push(flag);
+          }
+          flag++;
         }
-        flag++;
+        this.setState({
+          selectedRowKeys: tmpselectedRowKeys,
+        });
       }
-      this.setState({
-        selectedRowKeys:tmpselectedRowKeys,
-      });
-     }
-   })
- }
+    })
+  }
 
   handleDelete = indexID => {
-    const {dispatch}=this.props;
-    let params={
-      index:indexID
+    const { dispatch } = this.props;
+    let params = {
+      index: indexID
     }
     if (dispatch) {
       dispatch({
         type: 'todos/deleteEvent',
         payload: params,
-        callback:()=>{
-          const {success,msg}=this.props.datachange;
-          console.log("confirmmsg"+msg);
-          if(success){
+        callback: () => {
+          const { success, msg } = this.props.datachange;
+          console.log("confirmmsg" + msg);
+          if (success) {
             message.success(formatMessage({
               id: 'delete-event-success',
             }));
             this.reloadHandler(dispatch);
-          }else{
-             message.error(msg);
+          } else {
+            message.error(msg);
           }
         },
       });
     }
   };
 
-  reloadHandler=(dispatch)=>{
-    const {status}=this.state;
-    let tmpselectedRowKeys=[],flag=0;
-   dispatch({
-     type:'todos/fetchEventList',
-     payload:status,
-     callback:()=>{
-      for(let i=0;i<this.props.eventList.length;i++){
-        if(this.props.eventList[i].done===true){
-        tmpselectedRowKeys.push(flag);
+  reloadHandler = (dispatch) => {
+    const { status } = this.state;
+    const { eventList } = this.props;
+    let tmpselectedRowKeys = [], flag = 0;
+    dispatch({
+      type: 'todos/fetchEventList',
+      payload: status,
+      callback: () => {
+        for (let i = 0; i < eventList.length; i++) {
+          if (eventList[i].done === true) {
+            tmpselectedRowKeys.push(flag);
+          }
+          flag++;
         }
-        flag++;
+        this.setState({
+          selectedRowKeys: tmpselectedRowKeys,
+        });
       }
-      this.setState({
-        selectedRowKeys:tmpselectedRowKeys,
-      });
-     }
     })
-    
+
   }
 
   addEvent = value => {
-    const {dispatch,eventList}=this.props;
-    let maxIndex=eventList.length!=0?eventList[eventList.length-1].index+1:0;
-    let params={
-      index:maxIndex,
-      name:value,
-      done:false,
+    const { dispatch, eventList } = this.props;
+    let maxIndex = eventList.length != 0 ? eventList[eventList.length - 1].index + 1 : 0;
+    let params = {
+      index: maxIndex,
+      name: value,
+      done: false,
     }
     if (dispatch) {
       dispatch({
         type: 'todos/addEvent',
         payload: params,
-        callback:()=>{
-          const {success,msg}=this.props.datachange;
-          console.log("confirmmsg"+msg);
-          if(success){
+        callback: () => {
+          const { success, msg } = this.props.datachange;
+          console.log("confirmmsg" + msg);
+          if (success) {
             message.success(formatMessage({
               id: 'add-event-success',
             }));
             this.reloadHandler(dispatch);
-          }else{
-             message.error(msg);
+          } else {
+            message.error(msg);
           }
           this.props.form.resetFields(['input']);
         },
@@ -136,125 +137,125 @@ const EditableFormRow = Form.create()(EditableRow);
   }
 
   handleSave = row => {
-   const {dispatch}=this.props;
-   let params={
-     index:row.index,
-     name:row.name,
-   }
-   if (dispatch) {
-    dispatch({
-      type: 'todos/updateEvent',
-      payload: params,
-      callback:()=>{
-        const {success,msg}=this.props.datachange;
-        console.log("confirmmsg"+msg);
-        if(success){
-          message.success(formatMessage({
-            id: 'update-event-success',
-          }));
-          this.reloadHandler(dispatch);
-        }else{
-           message.error(msg);
-        }
-      },
-    });
-  }
-  };
-  
-  onChange=e=>{
-    console.log(`radio checked:${e.target.value}`);
-    const {dispatch}=this.props;
-    let tmpselectedRowKeys=[],flag=0;
-    let status;
-   if(e.target.value=='a'){
-       status=0;
-   }else if(e.target.value=='b'){
-       status=1;
-   }else{
-       status=2;
-   }
-   this.setState({
-     status,
-   });
-   dispatch({
-     type:'todos/fetchEventList',
-     payload:status,
-     callback:()=>{
-      for(let i=0;i<this.props.eventList.length;i++){
-        if(this.props.eventList[i].done===true){
-        tmpselectedRowKeys.push(flag);
-        }
-        flag++;
-      }
-      this.setState({
-        selectedRowKeys:tmpselectedRowKeys,
+    const { dispatch } = this.props;
+    let params = {
+      index: row.index,
+      name: row.name,
+    }
+    if (dispatch) {
+      dispatch({
+        type: 'todos/updateEvent',
+        payload: params,
+        callback: () => {
+          const { success, msg } = this.props.datachange;
+          console.log("confirmmsg" + msg);
+          if (success) {
+            message.success(formatMessage({
+              id: 'update-event-success',
+            }));
+            this.reloadHandler(dispatch);
+          } else {
+            message.error(msg);
+          }
+        },
       });
-     }
-   })
+    }
+  };
+
+  onChange = e => {
+    console.log(`radio checked:${e.target.value}`);
+    const { dispatch, eventList } = this.props;
+    let tmpselectedRowKeys = [], flag = 0;
+    let status;
+    if (e.target.value == 'a') {
+      status = 0;
+    } else if (e.target.value == 'b') {
+      status = 1;
+    } else {
+      status = 2;
+    }
+    this.setState({
+      status,
+    });
+    dispatch({
+      type: 'todos/fetchEventList',
+      payload: status,
+      callback: () => {
+        for (let i = 0; i < eventList.length; i++) {
+          if (this.props.eventList[i].done === true) {
+            tmpselectedRowKeys.push(flag);
+          }
+          flag++;
+        }
+        this.setState({
+          selectedRowKeys: tmpselectedRowKeys,
+        });
+      }
+    })
   }
 
-  onSelectChange=(record, selected, selectedRows, nativeEvent)=>{
+  onSelectChange = (record, selected, selectedRows, nativeEvent) => {
     console.log(`record:${record} selected:${selected}`);
-    const {dispatch}=this.props;
+    const { dispatch } = this.props;
     let params;
-    if(selected){
-      params={
-        index:record.index,
-        selected:true,
+    if (selected) {
+      params = {
+        index: record.index,
+        selected: true,
       }
-    }else{
-      params={ 
-        index:record.index,
-        selected:false,
+    } else {
+      params = {
+        index: record.index,
+        selected: false,
       }
     }
     dispatch({
-      type:"todos/changeDoneStatus",
-      payload:params,
-      callback:()=>{
-        const {success,msg}=this.props.datachange;
-        console.log("confirmmsg"+msg);
-        if(success){
+      type: "todos/changeDoneStatus",
+      payload: params,
+      callback: () => {
+        const { success, msg } = this.props.datachange;
+        console.log("confirmmsg" + msg);
+        if (success) {
           message.success(formatMessage({
             id: 'change-eventstatus-success',
           }));
           this.reloadHandler(dispatch);
-        }else{
-           message.error(msg);
+        } else {
+          message.error(msg);
         }
       },
     })
   }
 
-  clearCompleted=()=>{
-     const {dispatch}=this.props;
-     dispatch({
-      type:'todos/clearCompleted',
-      callback:()=>{
-        const {success,msg}=this.props.datachange;
-        console.log("confirmmsg"+msg);
-        if(success){
+  clearCompleted = () => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'todos/clearCompleted',
+      callback: () => {
+        const { success, msg } = this.props.datachange;
+        console.log("confirmmsg" + msg);
+        if (success) {
           message.success(formatMessage({
             id: 'clear-completed-success',
           }));
           this.reloadHandler(dispatch);
-        }else{
-           message.error(msg);
+        } else {
+          message.error(msg);
         }
       },
-     })
+    })
   }
 
   render() {
     const { selectedRowKeys } = this.state;
-    const {eventList,loading} = this.props;
+    const { eventList, loading } = this.props;
     const { getFieldDecorator } = this.props.form;
-    console.log("render",eventList);
+    console.log("render", eventList);
     const rowSelection = {
       selectedRowKeys,
-      onSelect:this.onSelectChange,
+      onSelect: this.onSelectChange,
       getCheckboxProps: record => ({
-       done:false,
+        done: false,
       }),
     };
     const components = {
@@ -281,51 +282,51 @@ const EditableFormRow = Form.create()(EditableRow);
     return (
       <div className={styles.main}>
         <Form>
-        <Form.Item style={{margin:0}}>
-          {getFieldDecorator('input')(
-          <Search
-            placeholder={formatMessage({
-              id: 'What-needs-to-be-done',
-            })}
-            enterButton={formatMessage({
-              id: 'Add',
-            })}
-            size="large"
-            onSearch={value=>this.addEvent(value)}
-          />)}
-        </Form.Item>
-        <Table 
-          loading={loading}
-          components={components}
-          rowClassName={() => 'editable-row'}
-          showHeader={false}
-          bordered={false}
-          pagination={ false } 
-          scroll={{ y: 400 }}
-          dataSource={eventList}
-          columns={columns}
-          rowSelection={rowSelection}
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator('input')(
+              <Search
+                placeholder={formatMessage({
+                  id: 'What-needs-to-be-done',
+                })}
+                enterButton={formatMessage({
+                  id: 'Add',
+                })}
+                size="large"
+                onSearch={value => this.addEvent(value)}
+              />)}
+          </Form.Item>
+          <Table
+            loading={loading}
+            components={components}
+            rowClassName={() => 'editable-row'}
+            showHeader={false}
+            bordered={false}
+            pagination={false}
+            scroll={{ y: 400 }}
+            dataSource={eventList}
+            columns={columns}
+            rowSelection={rowSelection}
           />
           <span>{eventList.length}<FormattedMessage id="items-left" /></span>
-          <Radio.Group onChange={this.onChange} defaultValue="a" style={{display:'inline-block',marginLeft:"10px"}}>
+          <Radio.Group onChange={this.onChange} defaultValue="a" style={{ display: 'inline-block', marginLeft: "10px" }}>
             <Radio.Button value="a"><FormattedMessage id="All" /></Radio.Button>
             <Radio.Button value="b"><FormattedMessage id="Active" /></Radio.Button>
             <Radio.Button value="c"><FormattedMessage id="Completed" /></Radio.Button>
           </Radio.Group>
-          <Button onClick={this.clearCompleted} style={{display:'float',float:'right'}}>
-             <FormattedMessage id="clearCompleted" />
+          <Button onClick={this.clearCompleted} style={{ display: 'float', float: 'right' }}>
+            <FormattedMessage id="clearCompleted" />
           </Button>
-       </Form>
-      </div> 
+        </Form>
+      </div>
     );
   }
 }
 
 
 export default connect(({ todos }) => ({
-  eventList:todos.eventList.obj,
-  datachange:todos.datachange,
-  loading:todos.loading,
+  eventList: todos.eventList.obj,
+  datachange: todos.datachange,
+  loading: todos.loading,
 }))(TodoList)
 
 
